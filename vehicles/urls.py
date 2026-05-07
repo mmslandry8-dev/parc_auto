6e4ff221f739d3b7f1c4f63d21717236392a5eb7
Landry MMs <mmslandry8@gmail.com>
2026-05-07 20:27:05 +0100

    CRUD véhicules complet + UI moderne

--- vehicles/urls.py
+++ vehicles/urls.py
@@ -4,9 +4,33 @@ from . import views
 urlpatterns = [
 
     path(
+        '',
+        views.vehicle_list,
+        name='vehicle_list'
+    ),
+
+    path(
         'add/',
         views.add_vehicle,
         name='add_vehicle'
+    ),
+
+    path(
+        '<int:pk>/',
+        views.vehicle_detail,
+        name='vehicle_detail'
+    ),
+
+    path(
+        '<int:pk>/delete/',
+        views.delete_vehicle,
+        name='delete_vehicle'
+    ),
+
+    path(
+        '<int:pk>/update/',
+        views.update_vehicle,
+        name='update_vehicle'
     ),
 
 ]
--- vehicles/views.py
+++ vehicles/views.py
@@ -5,6 +5,10 @@ from accounts.decorators import admin_required
 
 from .forms import VehicleForm
 
+from .models import Vehicle
+from django.contrib.auth.decorators import login_required
+from accounts.decorators import admin_required
+
 
 @login_required
 @admin_required
@@ -34,5 +38,82 @@ def add_vehicle(request):
         'vehicles/add_vehicle.html',
         {
             'form': form
+        }
+    )
+
+@login_required
+def vehicle_list(request):
+    """
+    Liste des véhicules
+    visible par tous les utilisateurs connectés
+    """
+
+    vehicles = Vehicle.objects.all()
+
+    return render(
+        request,
+        'vehicles/vehicle_list.html',
+        {
+            'vehicles': vehicles
+        }
+    )
+
+def vehicle_detail(request, pk):
+    """
+    Détails d'un véhicule
+    """
+
+    vehicle = Vehicle.objects.get(id=pk)
+
+    return render(
+        request,
+        'vehicles/vehicle_detail.html',
+        {
+            'vehicle': vehicle
+        }
+    )
+
+@admin_required
+def delete_vehicle(request, pk):
+    """
+    Supprimer un véhicule
+    """
+
+    vehicle = Vehicle.objects.get(id=pk)
+
+    vehicle.delete()
+
+    return redirect('vehicle_list')
+
+@admin_required
+def update_vehicle(request, pk):
+    """
+    Modifier un véhicule
+    """
+
+    vehicle = Vehicle.objects.get(id=pk)
+
+    form = VehicleForm(instance=vehicle)
+
+    if request.method == 'POST':
+
+        form = VehicleForm(
+            request.POST,
+            request.FILES,
+            instance=vehicle
+        )
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect('vehicle_list')
+
+    return render(
+        request,
+        'vehicles/update_vehicle.html',
+        {
+            'form': form,
+            'vehicle': vehicle
         }
     )
